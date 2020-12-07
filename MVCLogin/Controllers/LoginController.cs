5b40bce60dc2c9e9b96d32e6e2a8ab47cfc901a9
Koutheesh <koutheesh@gmail.com>
2020-12-07 10:56:52 +0530

    Add project files.

---
+++ MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using MVCLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(MVCLogin.Models.User userModel)
+        {
+            using (LoginDBEntities db = new LoginDBEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if(userDetails==null)
+                {
+                    userModel.LoginErrorMessage = "Wrong Username or Password";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+                
+        }
+    }
+}
